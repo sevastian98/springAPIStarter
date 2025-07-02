110041b3177027387b85d5d3193f51e15892baac
Sevastian <sevastian.vascan7@gmail.com>
2025-07-02 11:04:55 +0300

    Commit

---
+++ src/main/java/com/codewithmosh/controllers/MessageController.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.controllers;
+
+import com.codewithmosh.store.entities.Message;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @RequestMapping("/hello")
+    public Message sayHello() {
+        return new Message("Hello World");
+    }
+}
